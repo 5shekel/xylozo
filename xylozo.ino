fdb2d99c87fbfca59008528d5a5ee812cb591177
yair <yair99@gmail.com>
2015-06-11 04:34:14 +0200

    working with highlow array

--- xylozo.ino
+++ xylozo.ino
@@ -1,26 +1,31 @@
-  #include <Streaming.h>
-  #include "RF24.h" //http://tmrh20.github.io/ this is a manitned fork, the original is now radiohead
-  #include <SPI.h>
-  RF24 radio(7, 8);
-  typedef enum { role_xylo , role_hub } role_e;
-  role_e role = role_hub;// The role of the current running sketch
-  
-  byte addresses[][6] = {"1Node","2Node"};
+//xylo ttohw.tumblr.com
 
+#include <Streaming.h>
 
-  int sns00;
+#include "RF24.h" //http://tmrh20.github.io/ this is a manitned fork, the original is now radiohead
+#include <SPI.h>
+RF24 radio(7, 8);
+typedef enum { role_xylo , role_hub } role_e;
+role_e role = role_xylo;// The role of the current running sketch
+
+byte addresses[][6] = {"1Node","2Node"};
+int sns00;
+uint8_t piezoVal[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; 
 
 void setup() {
-    radio.begin();
-   Serial.begin(115200);
+  Serial.begin(115200);
 
-   if ( role == role_hub ) {  // Start listening
+  radio.begin();
+  radio.enableAckPayload();                         // We will be using the Ack Payload feature, so please enable it
+  radio.enableDynamicPayloads();                    // Ack payloads are dynamic payloads
+
+  if ( role == role_hub ) {  // Start listening
    radio.openWritingPipe(addresses[1]);
    radio.openReadingPipe(1,addresses[0]);
    radio.startListening();
- }
+    radio.printDetails();                             // Dump the configuration of the rf unit for debugging
 
-  if ( role == role_xylo )  { //setup remote pins
+  }else if ( role == role_xylo )  { //setup remote pins
    radio.openWritingPipe(addresses[0]);
    radio.openReadingPipe(1,addresses[1]);
    radio.stopListening(); 
@@ -30,16 +35,27 @@ void setup() {
 
 void loop() {
   if ( role == role_xylo)  {
-    sns00=analogRead(0);
-    bool ok = radio.write(&sns00, sizeof(int));
-    delay(1000);
-  }
-  if ( role == role_hub )
-   if ( radio.available() )
+    
+    for(int iii=0; iii < sizeof(piezoVal) ;iii+=2){
+      piezoVal[iii] = lowByte(analogRead(iii)); 
+      piezoVal[iii+1] = highByte(analogRead(iii)); 
+      }
+  
+    bool ok = radio.write(&piezoVal, sizeof(piezoVal));
+    delay(20);
+
+
+    }else if ( role == role_hub )
+    if ( radio.available() )
     {
       while(radio.available()){
-        radio.read(&sns00, sizeof(int));
-        Serial<<sns00<<endl;
+        radio.read(&piezoVal, sizeof(piezoVal));
+       
+        for(int iii=0; iii < sizeof(piezoVal); iii+=2)
+          Serial<<word(piezoVal[iii+1], piezoVal[iii])<<" ";
+         
+         Serial.println();
+        
       }
     }
-}
+  }
